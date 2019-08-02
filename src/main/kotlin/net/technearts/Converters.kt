3cb525d5e40465cabf76bf5386a27c522e7ae28b
Paulo Sudério <paulo.suderio@gmail.com>
2019-08-02 18:47:08 -0300

    Teste leitura do workbook

--- src/main/kotlin/net/technearts/Converters.kt
+++ src/main/kotlin/net/technearts/Converters.kt
@@ -1,6 +1,15 @@
 package net.technearts
 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import java.io.File
+import java.io.FileInputStream
 import java.math.BigDecimal
+import java.nio.file.Path
 
 val NumberConverter = {s: String -> BigDecimal(s)}
+
 infix fun <T> String.convert(converter: (String) -> T): T = converter(this)
+
+fun File.convert() = XSSFWorkbook(FileInputStream(this))
+
+fun Path.convert() = this.toFile().convert()
--- src/test/kotlin/net/technearts/ConvertersTest.kt
+++ src/test/kotlin/net/technearts/ConvertersTest.kt
@@ -1,7 +1,9 @@
 package net.technearts
 
 import io.kotlintest.shouldBe
+import io.kotlintest.shouldNotBe
 import io.kotlintest.specs.StringSpec
+import java.io.File
 import java.math.BigDecimal
 
 class ConvertersTest : StringSpec({
@@ -9,7 +11,13 @@ class ConvertersTest : StringSpec({
     "number conversion should return a BigDecimal" {
         "0" convert NumberConverter shouldBe BigDecimal.ZERO
     }
+
+    "workbook path should return a workbook" {
+        File(this::class.java.getResource("/test.xlsx").toURI()).convert() shouldNotBe null
+    }
 })
+
+
 
 
 
